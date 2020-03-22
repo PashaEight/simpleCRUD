c8d81cece1cef8863b4b3ff806f3d5692d210afa
PashaEight <8of8player@gmail.com>
2020-03-22 16:40:13 +0300

    added gitignore

--- src/main/java/MainApp/App.java
+++ src/main/java/MainApp/App.java
@@ -2,12 +2,8 @@ package MainApp;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Service service = context.getBean("service", Service.class);
         service.start();
