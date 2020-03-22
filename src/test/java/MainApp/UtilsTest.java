109a9aa05b2026be807a6f54de54b10cc875f573
PashaEight <8of8player@gmail.com>
2020-03-22 15:40:24 +0300

    added tests

---
+++ src/test/java/MainApp/UtilsTest.java
@@ -0,0 +1,18 @@
+package MainApp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilsTest {
+    @Test
+    public void commandWithParam() {
+        String commandWithParam = "insert -x -y";
+        Assert.assertEquals("insert", Utils.getCommand(commandWithParam));
+    }
+
+    @Test
+    public void rawCommand() {
+        String command = "read";
+        Assert.assertEquals("read", Utils.getCommand(command));
+    }
+}
