cdf7041ab2e9f4c9eeab2e662d027052eda02d6b
PashaEight <8of8player@gmail.com>
2020-03-26 02:23:10 +0300

    added .gradle to gitignore

---
+++ src/main/java/MainApp/AppConfig.java
@@ -0,0 +1,16 @@
+package MainApp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+    @Bean
+    public Service service(){
+        return new Service();
+    }
+}
